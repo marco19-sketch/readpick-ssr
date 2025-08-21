dd11ebe23e219c9bdda8727d3edf56c9c5b627e6
marco19-sketch <marco.brusca1970@gmail.com>
2025-08-21 07:22:18 +0200

    Adding back few functions at the time

--- src/app/RootClientWrapper.jsx
+++ src/app/RootClientWrapper.jsx
@@ -2,12 +2,15 @@
 
 import { Suspense } from "react";
 import AppContextProvider from "./components/AppContextProvider";
+import { usePathname } from "next/navigation";
 
 //testing
 // import { useTranslation, I18nextProvider } from "react-i18next";
 // import i18n from "../i18n-client";
-// import NavBar from "./components/NavBar";
-import { usePathname } from "next/navigation";
+import NavBar from "./components/NavBar";
+import BackToTop from "./components/BackToTop";
+import FooterLoader from "./components/FooterLoader";
+import SkipLink from "./components/SkipLink";
 
 
 
@@ -19,12 +22,24 @@ export default function RootClientWrapper({ children }) {
 
   return (
     // <I18nextProvider i18n={i18n}>
-      <AppContextProvider route={route}>
-        {/* {showNavBar && <NavBar t={t} />} */}
-        <main>
-          <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+    <AppContextProvider route={route}>
+      {" "}
+      <div className="layout-container">
+         <SkipLink />
+         {showNavBar && <NavBar />}
+        {/* // {showNavBar && <NavBar t={t} />} */}
+         {/* <LanguageSwitcher /> */}
+        {" "}
+        <main id="main-content" className="layout-main-content">
+           <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+          {" "}
         </main>
-      </AppContextProvider>
+         <BackToTop scrollContainerSelector="body" />
+         <FooterLoader />
+        {" "}
+      </div>
+      {" "}
+    </AppContextProvider>
     // </I18nextProvider>
   );
 }
