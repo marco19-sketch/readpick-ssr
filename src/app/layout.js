491c2cabca8705e55a5e6bb54e410ae40a4ab56b
marco19-sketch <marco.brusca1970@gmail.com>
2025-08-23 21:08:25 +0200

    Trying speedInsight from Vercel

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,7 @@
 import RootClientWrapper from "./RootClientWrapper";
 import "./globals.css";
 import { Inter } from "next/font/google";
-
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,15 +10,14 @@ export const metadata = {
   description: "A website for searching books",
 };
 
-
 export default function RootLayout({ children }) {
-
-
   return (
     <html lang="it" className={inter.className}>
+      <SpeedInsights />
       <body>
         <RootClientWrapper>
           {children}
+          <SpeedInsights />
         </RootClientWrapper>
       </body>
     </html>
