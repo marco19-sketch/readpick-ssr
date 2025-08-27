ce81412d0b8526047b92d401cd149d9a8e29afc4
marco19-sketch <marco.brusca1970@gmail.com>
2025-08-27 19:47:01 +0200

    Trying inline styles in splash page

--- src/app/splash/page.jsx
+++ src/app/splash/page.jsx
@@ -1,8 +1,9 @@
-import Image from 'next/image';
-import '@/styles/splash.css';
+import Image from "next/image";
+// import "@/styles/splash.css";
 
 export default function SplashScreen() {
-    return (
+  return (
+    <>
       <div className="layout-container">
         <Image
           src="/assets/images/laurentiu-1920-rot.jpg"
@@ -26,5 +27,73 @@ export default function SplashScreen() {
           <span className="dot-3">.</span>
         </h2>
       </div>
-    );
+      <style>{`
+        .splash-title {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: rgba(0, 0, 0, 0.849);
+  border-radius: 5px;
+  cursor: progress;
+}
+
+.splash-subtitle {
+  position: absolute;
+  top: 57%;
+  left: 50%;
+  transform: translateX(-50%);
+  color: white;
+  background-color: rgba(0, 0, 0, 0.849);
+  cursor: progress;
+}
+
+.layout-main-content.background {
+  margin: 1vh auto;
+  position: fixed;
+  width: auto;
+  height: 98vh;
+  max-height: 98%;
+  object-fit: cover;
+  cursor: progress;
+}
+
+.dot-1 {
+  color: white;
+  animation: dot-flash 1.5s 1s infinite ;
+}
+
+.dot-2 {
+  color: white;
+ animation: dot-flash 1.5s 0.5s infinite ;
+}
+
+.dot-3 {
+  color: white;
+  animation: dot-flash 1.5s  infinite ;
+}
+
+@keyframes dot-flash {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+
+@media screen and (min-width: 600px) {
+  .layout-main-content.background {
+    width: 100vw;
+    max-width: 100%;
+    height: auto;
+    object-fit: cover;
+  }
+}
+`}</style>
+    </>
+  );
 }
