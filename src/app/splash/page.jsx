f4efa814e68a14e9deb559bbeb9a5a58a7d97fc3
marco19-sketch <marco.brusca1970@gmail.com>
2025-08-28 16:51:02 +0200

    changed image format

--- src/app/splash/page.jsx
+++ src/app/splash/page.jsx
@@ -18,7 +18,7 @@ export default function SplashScreen() {
       <>
         <div className="layout-container">
           <Image
-            src="/assets/images/laurentiu-1920-rot.jpg"
+            src="/assets/images/laurentiu-1920-rot.avif"
             sizes="(max-width: 480px) 480px,
 >>>>>>> f5ea8d5 (Centered image position on mobile and inlined splash css)
          (max-width: 768px) 768px,
