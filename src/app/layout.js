271cf5e15249506ac35c67eea17b6f9157296573
marco19-sketch <marco.brusca1970@gmail.com>
2025-08-29 08:18:04 +0200

    Adding a preload to the Inter font in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import RootClientWrapper from "./RootClientWrapper";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Head from "next/head";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -16,6 +17,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="it" className={inter.className}>
+      <Head>
+        {/* The href must be the full file path from your global.css */}
+        <link
+          rel="preload"
+          href="/_next/static/media/e4af272ccee01ff0-s.p.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin=""
+        />
+      </Head>
       <SpeedInsights />
       <body>
         <RootClientWrapper>
